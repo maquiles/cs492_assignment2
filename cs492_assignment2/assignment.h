de778f89ea2574b6906bcb4a192da17393dac7ef
maquiles <mattaquiles@gmail.com>
2018-04-09 16:53:12 -0400

    removing some comments

--- cs492_assignment2/assignment.h
+++ cs492_assignment2/assignment.h
@@ -53,16 +53,3 @@ t_program *global_programs;
 int global_page_count;
 int global_program_count;
 int global_pageID;
-
-/*
-int std_param_check(char*, int);
-void read_file(char*);
-int get_instance_count(char*);
-void run_programs();
-void demand_fifo_replacement();
-void demand_clock_replacement();
-void demand_lru_replacement();
-void prepaging_fifo_replacement();
-void prepaging_clock_replacement();
-void prepaging_lru_replacement();
-*/
